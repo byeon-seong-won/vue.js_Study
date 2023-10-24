fee435a5ba43b49b99a47e15f1c25d63fd98c2cf
Seongwon <sngoneee@gmail.com>
2023-10-24 10:44:30 +0900

    study_1024

--- vuestudy/src/App.vue
+++ vuestudy/src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <h4>XX 원룸</h4>
+      <p>XX 만원</p>
+    </div>
+    <div>
+      <h4>XX 원룸</h4>
+      <p>XX 만원</p>
+    </div>
+  </div>
 </template>
 
 <script>
