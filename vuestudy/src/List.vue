2facdf0ef47f52caf03ecd67e1f8484af547b288
Seongwon <sngoneee@gmail.com>
2023-10-25 14:43:02 +0900

    blog 만들기

---
+++ vuestudy/src/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h5>{{list.title}}</h5>
+    <p>{{list.date}}</p>
+   </div>
+</template>
+
+<script>
+export default {
+    props : {
+      list : Object
+    },
+    // methods : {
+    //     clickEvent() {
+    //         this.$emit('openModal', this.원룸.id)
+    //     }
+    // }
+}
+</script>
