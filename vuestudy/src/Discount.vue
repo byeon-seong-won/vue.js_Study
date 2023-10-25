c842eefb3814c5eb77f28d27c1ca16c1f18c4775
Seongwon <sngoneee@gmail.com>
2023-10-25 10:32:02 +0900

    validation

--- vuestudy/src/Discount.vue
+++ vuestudy/src/Discount.vue
@@ -3,13 +3,14 @@
       <h4>지금 결제하면 20% 할인</h4>
     </div>
 </template>
-
 <script>
 export default {
   name : 'Discount',
-
+  // props : {
+  //   오브젝트 : Object
+  // }
 }
 </script>
 <style>
-  
+  .discount {border: 1px solid #000;}
 </style>
