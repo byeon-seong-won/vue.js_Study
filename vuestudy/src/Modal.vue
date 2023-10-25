bbd3167015fbd1c22c47af54959bfb74d2f427e0
Seongwon <sngoneee@gmail.com>
2023-10-25 10:32:07 +0900

    validation

--- vuestudy/src/Modal.vue
+++ vuestudy/src/Modal.vue
@@ -4,6 +4,8 @@
         <img :src="원룸들[idx].image" class="room-img">
         <h4>{{원룸들[idx].title}}</h4>
         <p>{{원룸들[idx].price}}만원</p>
+        <input v-model.number="month" placeholder="가격입력!!">
+        <p>최종가격 : {{month * 10}} 원</p>
         <button @click="closeModal()">닫기</button>
       </div>
     </div>
@@ -20,6 +22,18 @@ export default {
   methods : {
     closeModal() {
         this.$emit('closeModal')
+    }
+  },
+  data() {
+    return {
+      month : 0,
+    }
+  },
+  watch : {
+    month(a) {
+      if(a > 12) {
+        alert("경고문임")
+      }
     }
   }
 }
