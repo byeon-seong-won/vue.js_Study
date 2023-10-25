d632cfe72a4781751c6da6a86b08a4f25301136f
Seongwon <sngoneee@gmail.com>
2023-10-25 18:00:41 +0900

    blog 수정

--- vuestudy/src/components/Listcomp.vue
+++ vuestudy/src/components/Listcomp.vue
@@ -2,17 +2,22 @@
   <div>
     <h5>{{lists[0].title}}</h5>
     <p>{{lists[0].date}}</p>
+    <router-link to="/detail/0">이동</router-link>
   </div>
   <div>
     <h5>{{lists[1].title}}</h5>
     <p>{{lists[1].date}}</p>
+    <router-link to="/detail/1">이동</router-link>
   </div>
   <div>
     <h5>{{lists[2].title}}</h5>
     <p>{{lists[2].date}}</p>
+    <router-link to="/detail/2">이동</router-link>
   </div>
 </template>
 <script>
+
+
 export default {
   name : 'list',
   props : {
@@ -21,5 +26,5 @@ export default {
 }
 </script>
 <style>
-
+  router-link {background-color: #fff;color: #333;border: 1px solid #fff;}
 </style>
