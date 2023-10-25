5ce41245d356abc0b3db1a9d9243fa806751e848
Seongwon <sngoneee@gmail.com>
2023-10-25 14:43:14 +0900

    blog 만들기

--- vuestudy/src/assets/data.js
+++ vuestudy/src/assets/data.js
@@ -1,42 +1,18 @@
-export default [{
-  id : 0,
-  title: "Sinrim station 30 meters away",
-  image: "https://codingapple1.github.io/vue/room0.jpg",
-  content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
-  price: 700000
-  },
-  {
-  id : 1,
-  title: "Changdong Aurora Bedroom(Queen-size)",
-  image: "https://codingapple1.github.io/vue/room1.jpg",
-  content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
-  price: 450000
-  },
-  {
-  id : 2,
-  title: "Geumsan Apartment Flat",
-  image: "https://codingapple1.github.io/vue/room2.jpg",
-  content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
-  price: 780000
-  },
-  {
-  id : 3,
-  title: "Double styled beds Studio Apt",
-  image: "https://codingapple1.github.io/vue/room3.jpg",
-  content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
-  price: 550000
-  },
-  {
-  id : 4,
-  title: "MyeongIl Apartment flat",
-  image: "https://codingapple1.github.io/vue/room4.jpg",
-  content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
-  price: 680000
-  },
-  {
-  id : 5,
-  title: "Banziha One Room",
-  image: "https://codingapple1.github.io/vue/room5.jpg",
-  content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
-  price: 370000
-}];
+export default [
+  { 
+    title : '첫 째 프로젝트 : 허위매물 전문 부동산 앱',
+    content : 'Vue를 이용하면 비누같이 매끈한 앱을 만들 수 있습니다',
+    date : 'September 24, 2021',
+    number : 0
+  },{ 
+    title : '둘 째 프로젝트 : 오마카세 배달 앱',
+    content : '음식이 아니라 셰프를 직접 배달해드립니다',
+    date : 'October 20, 2020',
+    number : 1
+  },{ 
+    title : '셋 째 프로젝트 : 현피 앱',
+    content : '거리를 설정하면 가장 가까운 파이터를 소개해드려요! 서로 싸워보세요',
+    date : 'April 24, 2019',
+    number : 2
+  }
+]
