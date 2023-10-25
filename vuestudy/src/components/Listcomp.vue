1636d1da7cfa7a4b11496fe9610cac55a82617a2
Seongwon <sngoneee@gmail.com>
2023-10-25 16:57:36 +0900

    blog 수정

---
+++ vuestudy/src/components/Listcomp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h5>{{lists[0].title}}</h5>
+    <p>{{lists[0].date}}</p>
+  </div>
+  <div>
+    <h5>{{lists[1].title}}</h5>
+    <p>{{lists[1].date}}</p>
+  </div>
+  <div>
+    <h5>{{lists[2].title}}</h5>
+    <p>{{lists[2].date}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name : 'list',
+  props : {
+    lists : Array
+  }
+}
+</script>
+<style>
+
+</style>
