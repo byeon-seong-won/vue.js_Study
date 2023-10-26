30cde7acb237b2f7e6c8a700daf6bed73bad9b30
Seongwon <sngoneee@gmail.com>
2023-10-26 11:26:06 +0900

    blod 수정

--- vuestudy/src/components/Listcomp.vue
+++ vuestudy/src/components/Listcomp.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
-    <h5>{{lists[0].title}}</h5>
-    <p>{{lists[0].date}}</p>
-    <router-link to="/detail/0">이동</router-link>
-  </div>
-  <div>
-    <h5>{{lists[1].title}}</h5>
-    <p>{{lists[1].date}}</p>
-    <router-link to="/detail/1">이동</router-link>
-  </div>
-  <div>
-    <h5>{{lists[2].title}}</h5>
-    <p>{{lists[2].date}}</p>
-    <router-link to="/detail/2">이동</router-link>
+  <div class="listcont">
+    <div>
+      <div>
+        <h5>{{lists[0].title}}</h5>
+        <p>{{lists[0].date}}</p>
+        <router-link to="/detail/0">이동</router-link>
+      </div>
+      <div>
+        <h5>{{lists[1].title}}</h5>
+        <p>{{lists[1].date}}</p>
+        <router-link to="/detail/1">이동</router-link>
+      </div>
+      <div>
+        <h5>{{lists[2].title}}</h5>
+        <p>{{lists[2].date}}</p>
+        <router-link to="/detail/2">이동</router-link>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -26,5 +30,17 @@ export default {
 }
 </script>
 <style>
-  router-link {background-color: #fff;color: #333;border: 1px solid #fff;}
+    /* list page */
+  .listcont {
+    width: calc(100% - 80px);
+    min-height: 1200px;
+    height: calc(100% - 80px);
+    margin: 0 auto;
+    border-radius: 2rem;
+    overflow: hidden;
+    padding: 70px;
+  }
+  .listcont>div {border:3px solid red;}
+
+
 </style>
