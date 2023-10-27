3120377d6db58bc7a72caf3e398da8b35a52f84c
seong <sngoneee@gmail.com>
2023-10-27 23:03:19 +0900

    blog 수정

---
+++ vuestudy/src/components/filterboxcomp.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+    <div @click="fire" :class="filter + ' filter-item'" :style="`background-Image : url(${img})`">
+    </div> 
+</template>
+
+
+
+
+<script>
+export default {
+    name : 'filterbox',
+    props : {
+        img : String,
+        filter : String
+    },
+    methods : {
+        fire() {
+            this.emitter.emit('click', this.filter)
+        }
+    }
+
+}
+
+</script>
+
+
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color : white;
+  background-size: cover;
+  background-position : center;
+}
+</style>
