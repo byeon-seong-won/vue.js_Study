731b230c6f0fbe269335969b3ed1d9afd99032d5
seong <sngoneee@gmail.com>
2023-10-28 23:58:56 +0900

    blog 수정

--- vuestudy/src/components/Listcomp.vue
+++ vuestudy/src/components/Listcomp.vue
@@ -27,23 +27,8 @@ export default {
 
 <style>
 .post {width: 100%;display: flex;justify-content: space-between;align-items: center;}
-.profile {
-  /* background-image: url("https://picsum.photos/100?random=0"); */
-  width: 30px;
-  height: 30px;
-  background-size: 100%;
-  border-radius: 50%;
-  float: left;
-}
-.profile-name {
-  display: block;
-  float: left;
-  padding-left: 10px;
-  padding-top: 7px;
-  font-size: 14px;
-}
-.post-header {height: 30px;padding: 10px;}
-.post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: cover;flex: 3;}
-.post-content {padding-left: 15px;padding-right: 15px;font-size: 14px;}
+st-header {height: 30px;padding: 10px;}
+.post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;flex: 3;background-repeat: no-repeat;border: 1px solid #000;}
+.post-content {padding-left: 15px;padding-right: 15px;font-size: 14px;flex: 1;border: 1px solid #000;}
 .date {font-size: 11px;color: grey;margin-top: -8px;} 
   </style>
