9bc70cd04b9c06a5cc5cf93ddd9934bb738d7525
seong <sngoneee@gmail.com>
2023-10-29 17:54:52 +0900

    blog 수정

--- vuestudy/src/router.js
+++ vuestudy/src/router.js
@@ -1,5 +1,4 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Detail from './components/Detailcomp.vue'
 import Main from './components/Maincomp.vue'
 import Post from './components/Postcomp.vue'
 
@@ -13,10 +12,6 @@ const routes = [
     path: "/list",
     component: Post,
     name : 'Post'
-  },
-  {
-    path: "/detail/:id",
-    component: Detail
   }
 ];
 
