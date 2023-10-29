e0ddb0ba8b618a54f6b7ed4284f077062cdbec30
seong <sngoneee@gmail.com>
2023-10-29 13:31:40 +0900

    blog 수정

--- vuestudy/src/components/Menucomp.vue
+++ vuestudy/src/components/Menucomp.vue
@@ -19,7 +19,7 @@ export default {
 </script>
 
 <style>
-.menu {width: calc(100% - 80px);border: 1px solid #000;margin: 0 auto;display: flex;justify-content: space-between;align-items: center;background : transparent;padding : 15px;border-radius : 5px;text-align: center;}
+.menu {width: calc(100% - 80px);margin: 0 auto;display: flex;justify-content: space-between;align-items: center;background : transparent;padding : 15px;border-radius : 5px;text-align: center;}
 .menu>h1>a {color : #444;padding : 10px;font-weight: bold;font-size: 20px;display: inline-block;text-align: left;font-family: 'roboto', sans-serif;}
 .menu>div {display: flex;flex: 3;justify-content: end;}
 .menu>div>a {color: #444;font-size: 18px;letter-spacing: -0.05em;font-weight: bold;}
