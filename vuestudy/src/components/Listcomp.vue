b91e133cba86961efbe10142125216c64fcb5529
seong <sngoneee@gmail.com>
2023-10-29 17:28:07 +0900

    blog 수정

--- vuestudy/src/components/Listcomp.vue
+++ vuestudy/src/components/Listcomp.vue
@@ -2,11 +2,9 @@
 
 <template>
   <div class="post">
+    <p class="date">{{post.date}}</p>
     <div class="post-body" :style="{ backgroundImage : `url('${post. postImage}')`}" :class="`${post.filter}`"></div>
-    <div class="post-content">
-      <p>{{post.content}}</p>
-      <p class="date">{{post.date}}</p>
-    </div>
+    <p>{{post.content}}</p>
   </div> 
 </template>
 
@@ -26,9 +24,9 @@ export default {
 
 
 <style>
-.post {width: 100%;display: flex;justify-content: space-between;align-items: center;}
+.post {width: 100%;display: flex;justify-content: space-between;align-items: center;flex-direction: column;}
 st-header {height: 30px;padding: 10px;}
-.post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;flex: 3;background-repeat: no-repeat;border: 1px solid #000;}
+.post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;}
 .post-content {padding-left: 15px;padding-right: 15px;font-size: 14px;flex: 1;border: 1px solid #000;}
 .date {font-size: 11px;color: grey;margin-top: -8px;} 
   </style>
