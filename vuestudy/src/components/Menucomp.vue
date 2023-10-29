a1609b0dcc3944fbb426853581b4189f31a6ece4
seong <sngoneee@gmail.com>
2023-10-30 00:19:37 +0900

    blog 수정

--- vuestudy/src/components/Menucomp.vue
+++ vuestudy/src/components/Menucomp.vue
@@ -4,7 +4,7 @@
         <router-link to="/">BYSEONG</router-link>
       </h1>
       <div>
-        <router-link to="/list">블로그 글 보러가기</router-link>
+        <router-link to="/post">Blog List</router-link>
       </div>
     </div>
 </template>
@@ -21,8 +21,8 @@ export default {
 .menu {width: calc(100% - 80px);margin: 0 auto;display: flex;justify-content: space-between;align-items: center;background : transparent;padding : 15px;border-radius : 5px;text-align: center;}
 .menu>h1>a {color : #444;padding : 10px;font-weight: bold;font-size: 20px;display: inline-block;text-align: left;font-family: 'roboto', sans-serif;}
 .menu>div {display: flex;flex: 3;justify-content: end;}
-.menu>div>a {color: #444;font-size: 18px;letter-spacing: -0.05em;font-weight: bold;}
-.menu>div>a:hover {color: #888;}
+.menu>div>a {border: 0.15rem solid #444;color: #444;font-size: 18px;letter-spacing: -0.05em;font-weight: bold;padding: 0.5rem 1.2rem;border-radius: 10rem;}
+.menu>div>a:hover {color: #fff;background-color: #333;}
 .menu>div>a:last-child {display: inline-block;margin-left: 50px;}
 
 </style>
