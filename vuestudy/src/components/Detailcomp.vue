cbaa3544aae51049eda699c912c99560d08cf3d3
seong <sngoneee@gmail.com>
2023-10-30 23:29:48 +0900

    blog 수정

--- vuestudy/src/components/Detailcomp.vue
+++ vuestudy/src/components/Detailcomp.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="detail">
+        <p class="date">{{posts[$route.params.id].date}} 의 일상</p>
         <div class="post-body" :style="{ backgroundImage : `url('${posts[$route.params.id]. postImage}')`}" :class="`${posts[$route.params.id].filter}`"></div>
         <div class="post-content">
-            <p class="date">{{posts[$route.params.id].date}}</p>
             <p>{{posts[$route.params.id].content}}</p>
         </div>
     </div> 
@@ -20,16 +20,16 @@
 <style>
 .detail {
     width: calc(100% - 80px);
-    max-width: 1200px;
-    min-height: 1200px;
+    max-width: 1000px;
+    min-height: 800px;
     height: calc(100% - 80px);
     margin: 70px auto 0;
     position: relative;
-    border: 3px solid red;
-    display: flex;justify-content: space-between;align-items: center;flex-direction: column;row-gap: 50px;}
+    text-align: left;
+    }
 st-header {height: 30px;padding: 10px;}
-.detail .post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: 100%;background-repeat: no-repeat;width: 100%;border: 1px solid #000;flex: 3;border: 2px solid blue;}
-.detail .post-content {padding-left: 15px;padding-right: 15px;font-size: 14px;flex: 1;border: 1px solid #000;flex: 2;border: 2px solid green;}
-.detail .post-content p {font-size: 20px;}
-.detail .date {font-size: 16px;color: grey;} 
+.detail .post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;flex: 3;}
+.detail .post-content {padding:20px; font-size: 14px;flex: 2;background-color: #eee;}
+.detail .post-content p {font-size: 18px;}
+.detail .date {font-size: 22px;color: #555;margin-bottom: 40px;} 
 </style>
