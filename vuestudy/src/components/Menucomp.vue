af95b5c241e0b6c10d00ef4414c3cebb1d09b1f7
Seongwon <sngoneee@gmail.com>
2023-10-31 17:50:00 +0900

    blog 수정

--- vuestudy/src/components/Menucomp.vue
+++ vuestudy/src/components/Menucomp.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="menu">
-      <h1>
-        <router-link to="/">BYSEONG</router-link>
-      </h1>
-      <div>
-        <router-link to="/post" @click="$emit('menuClick')">Blog List</router-link>
+      <div class="nav">
+        <h1>
+          <router-link to="/">BYSEONG</router-link>
+        </h1>
+        <div>
+          <router-link to="/post" @click="$emit('menuClick')">Blog List</router-link>
+        </div>
       </div>
     </div>
 </template>
@@ -18,11 +20,32 @@ export default {
 </script>
 
 <style>
-.menu {width: calc(100% - 80px);margin: 0 auto;display: flex;justify-content: space-between;align-items: center;background : transparent;padding : 15px;border-radius : 5px;text-align: center;}
-.menu>h1>a {color : #444;padding : 10px;font-weight: bold;font-size: 20px;display: inline-block;text-align: left;font-family: 'roboto', sans-serif;}
-.menu>div {display: flex;flex: 3;justify-content: end;}
-.menu>div>a {border: 0.15rem solid #444;color: #444;font-size: 18px;letter-spacing: -0.05em;font-weight: bold;padding: 0.5rem 1.2rem;border-radius: 10rem;}
-.menu>div>a:hover {color: #fff;background-color: #333;}
-.menu>div>a:last-child {display: inline-block;margin-left: 50px;}
+.menu {
+  position: fixed;
+  width: 100%;
+  box-sizing: border-box;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+}
+.menu>div {
+  border: 0.13rem solid #888;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  width: calc(100% - 150px);
+  padding: 15px;
+  border-radius: 1.3rem;
+  text-align: center;
+  margin: 0 auto;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+}
+.menu>div>h1>a {color : #444;padding : 10px;font-weight: bold;font-size: 20px;display: inline-block;text-align: left;font-family: 'roboto', sans-serif;}
+.menu>div>div {display: flex;flex: 3;justify-content: end;}
+.menu>div>div>a {box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;border: 0.15rem solid #444;color: #444;font-size: 18px;letter-spacing: -0.05em;font-weight: bold;padding: 0.5rem 1.2rem;border-radius: 10rem;}
+.menu>div>div>a:hover {color: #fff;background-color: #333;}
+.menu>div>div>a:last-child {display: inline-block;margin-left: 50px;}
 
 </style>
