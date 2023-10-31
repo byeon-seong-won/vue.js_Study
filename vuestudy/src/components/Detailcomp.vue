409c7f250edbdc881c0ff22de77141ec50a63ccd
Seongwon <sngoneee@gmail.com>
2023-10-31 17:49:41 +0900

    blog 수정

--- vuestudy/src/components/Detailcomp.vue
+++ vuestudy/src/components/Detailcomp.vue
@@ -1,36 +1,42 @@
 <template>
     <div class="detail">
-        <p class="date">{{posts[$route.params.id].date}} 의 일상</p>
-        <button @click="modifystate=true">수정하기</button>
+        <h1>MY DAILY LIFE</h1>  
         <div v-if="modifystate == false">
+            <p class="date">{{posts[$route.params.id].date}}</p>
             <div class="post-body" :style="{ backgroundImage : `url('${posts[$route.params.id]. postImage}')`}" :class="`${posts[$route.params.id].filter}`"></div>
             <div class="post-content">
                 <p>{{posts[$route.params.id].content}}</p>
             </div>
+            <div class="btnClass">
+                <button @click="modifystate=true">수정하기</button>
+                <router-link to="/post">목록으로 돌아가기</router-link>
+            </div>
         </div>
+        
 
         <div v-if="modifystate == true">
-            <p>수정할 이미지를 업로드해주세요!</p>
+            <h4 class="stepTitle">수정할 이미지를 업로드해주세요!</h4>
             <ul>
               <input @change="modiUd" type="file" id="file"/>
               <label for="file" class="input-plus"></label>
             </ul>
-              <div class="upload-image" :style="{backgroundImage : `url('${modiImg}')` }" :class="modiFilter">
-                <!-- <div v-if="uploaded == false" class="uploadPz">
-                  <img src=".././assets/notfound.png">
-                  <h4>이미지를 업로드해주세요!</h4>
-                </div> -->
+            <div class="upload-image" :style="{backgroundImage : `url('${modiImg}')` }" :class="modiFilter">
+              <div v-if="uploaded == false" class="uploadPz">
+                <img src=".././assets/notfound.png">
+                <h4>수정할 이미지를 업로드해주세요!</h4>
               </div>
-              <div class="filters">
-                  <p>필터 선택하기</p>
-                  <FilterBox :img="modiImg" :filter="filters[i]" v-for="(filter,i) in filters" :key="i">
-                    <!-- <template v-slot:b><span>데이터2</span></template> -->
-                  </FilterBox>
-              </div>
-              <textarea class="write-box" @input="modiWrite">
-
-              </textarea>
-            <button @click="modify" class="newBtn">수정완료</button>
+            </div>
+            <div class="filters">
+                <p>필터 선택하기 <span class="xi-arrow-down"></span></p>
+                <FilterBox :img="modiImg" :filter="filters[i]" v-for="(filter,i) in filters" :key="i">
+                  <!-- <template v-slot:b><span>데이터2</span></template> -->
+                </FilterBox>
+            </div>
+            <textarea class="write-box modiWrite-box" @input="modiWrite"></textarea>
+            <div class="btnClass">
+                <button @click="modify" class="newBtn">수정완료</button>
+                <router-link to="/post">취소하기</router-link>
+            </div>
         </div>
     </div> 
 </template>
@@ -50,7 +56,8 @@ import FilterBox from './filterboxcomp.vue'
               "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson", 
               "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", 
               "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"
-            ]
+            ],
+            uploaded : false
           }
         },
         props : {
@@ -69,6 +76,7 @@ import FilterBox from './filterboxcomp.vue'
               let 수정파일 = e.target.files;
               let modiurl = URL.createObjectURL(수정파일[0]);
               this.modiImg = modiurl;
+              this.uploaded = true;
             },
             modiWrite($event) {
               this.modiCont = $event.target.value
@@ -96,16 +104,20 @@ import FilterBox from './filterboxcomp.vue'
 <style>
 .detail {
     width: calc(100% - 80px);
-    max-width: 1000px;
+    max-width: 1200px;
     min-height: 800px;
-    height: calc(100% - 80px);
-    margin: 70px auto 0;
-    position: relative;
     text-align: left;
+    margin: 0 auto;
+    position: relative;
+    padding: 200px 0;
     }
+    .detail>h1 {font-size: 6rem;text-align: left;font-family: 'Acme', sans-serif;color: #333;text-transform: uppercase;margin-bottom: 50px;}
 st-header {height: 30px;padding: 10px;}
+.detail .btnClass {text-align: right;margin-top: 50px;}
+.detail .btnClass a {margin-left: 10px;font-size: 14px;display: inline-block;background: crimson;color: #fff;border: none;padding: 10px;border-radius: 10px;cursor: pointer;}
 .detail .post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;flex: 3;}
 .detail .post-content {padding:20px; font-size: 14px;flex: 2;background-color: #eee;}
 .detail .post-content p {font-size: 18px;}
-.detail .date {font-size: 22px;color: #555;margin-bottom: 40px;} 
+.detail .date {font-size: 20px;color: #777;margin-bottom: 10px;font-weight: 300;} 
+.modiWrite-box {margin-top: 30px;}
 </style>
