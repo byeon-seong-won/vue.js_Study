b818f415ac1641d7d74568a59f021abd465404a0
Seongwon <sngoneee@gmail.com>
2023-11-01 10:33:54 +0900

    blog 수정

--- vuestudy/src/components/Listcomp.vue
+++ vuestudy/src/components/Listcomp.vue
@@ -10,11 +10,7 @@
   </div> 
 </template>
 
-
-
-
 <script>
-
 export default {
   name : '',
   props : {
@@ -22,9 +18,7 @@ export default {
     idx : Number
   }
 }
-
 </script>
-
 
 <style>
 .post {width: 100%;text-align: left;height: 100%;}
@@ -32,8 +26,7 @@ export default {
 .xi-close:before {font-size: 24px;}
 .post .detailBtn {text-align: center;margin-top: 30px;}
 .post .detailBtn button {background: #333;width: 25%;padding: 1rem 0;}
-.post-body {/* background-image: url("https://picsum.photos/600?random=0"); */min-height: 300px;max-height: 400px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;margin: 20px 0;background-color: #111;}
+.post-body {min-height: 300px;max-height: 400px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;margin: 20px 0;background-color: #111;}
 .post-content {padding: 20px;font-size: 16px;background-color: #fff;border-radius: 20px;min-height: 300px;}
 .date {font-size: 16px;color: #333;margin-top: -8px;font-weight: bold;} 
-
-  </style>
+</style>
