2c693dca129a9307354d3f91d4a7c4ad09d60cc4
Seongwon <sngoneee@gmail.com>
2023-11-01 10:33:44 +0900

    blog 수정

--- vuestudy/src/components/Detailcomp.vue
+++ vuestudy/src/components/Detailcomp.vue
@@ -12,8 +12,6 @@
                 <router-link to="/post">목록으로 돌아가기</router-link>
             </div>
         </div>
-        
-
         <div v-if="modifystate == true">
             <h4 class="stepTitle">수정할 이미지를 업로드해주세요!</h4>
             <ul>
@@ -28,11 +26,9 @@
             </div>
             <div class="filters">
                 <p>필터 선택하기 <span class="xi-arrow-down"></span></p>
-                <FilterBox :img="modiImg" :filter="filters[i]" v-for="(filter,i) in filters" :key="i">
-                  <!-- <template v-slot:b><span>데이터2</span></template> -->
-                </FilterBox>
+                <FilterBox :img="modiImg" :filter="filters[i]" v-for="(filter,i) in filters" :key="i"></FilterBox>
             </div>
-            <textarea class="write-box modiWrite-box" @input="modiWrite"></textarea>
+            <textarea class="write-box modiWrite-box" @input="modiWrite" placeholder="블로그에 업로드 할 내용을 입력해주세요." onfocus="this.placeholder = ''" onblur="this.placeholder = '블로그에 업로드 할 내용을 입력해주세요.'"></textarea>
             <div class="btnClass">
                 <button @click="modify" class="newBtn">수정완료</button>
                 <router-link to="/post">취소하기</router-link>
@@ -89,10 +85,7 @@ import FilterBox from './filterboxcomp.vue'
                     content: this.modiCont,
                     filter: this.modiFilter
                 };
-                // console.log("수정게시물" + JSON.stringify(수정게시물))
                 this.emitter.emit("modify", 수정게시물)
-                // console.log("detail 수정게시물" + JSON.stringify(수정게시물) + this.idx)
-                // console.log(this.$route.params.id)
                 this.modifystate = false;
                 alert("수정이 완료되었습니다.")
                 this.$router.push({name: 'Post'})
@@ -103,21 +96,26 @@ import FilterBox from './filterboxcomp.vue'
 
 <style>
 .detail {
-    width: calc(100% - 80px);
-    max-width: 1200px;
-    min-height: 800px;
-    text-align: left;
-    margin: 0 auto;
-    position: relative;
-    padding: 250px 0;
-    }
-    .detail>h1 {font-size: 6rem;text-align: left;font-family: 'Acme', sans-serif;color: #333;text-transform: uppercase;margin-bottom: 50px;}
-st-header {height: 30px;padding: 10px;}
+  width: calc(100% - 80px);
+  max-width: 1200px;
+  min-height: 800px;
+  text-align: left;
+  margin: 0 auto;
+  position: relative;
+  padding: 200px 0;
+}
+.detail>h1 {font-size: 6rem;text-align: left;font-family: 'Acme', sans-serif;color: #333;text-transform: uppercase;margin-bottom: 50px;}
 .detail .btnClass {text-align: right;margin-top: 50px;}
 .detail .btnClass a {margin-left: 10px;font-size: 14px;display: inline-block;background: crimson;color: #fff;border: none;padding: 10px;border-radius: 10px;cursor: pointer;}
-.detail .post-body {/* background-image: url("https://picsum.photos/600?random=0"); */height: 450px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;flex: 3;}
-.detail .post-content {padding:20px; font-size: 14px;flex: 2;background-color: #eee;}
+.detail .post-body {height: 450px;background-position: center;background-size: contain;background-repeat: no-repeat;width: 100%;flex: 3;}
+.detail .post-content {padding:20px; font-size: 14px;flex: 2;background-color: whitesmoke;}
 .detail .post-content p {font-size: 18px;}
 .detail .date {font-size: 20px;color: #777;margin-bottom: 10px;font-weight: 300;} 
 .modiWrite-box {margin-top: 30px;}
+
+
+@media (max-width: 1920px) {
+  .detail>h1 {font-size: 5rem;}
+}
+
 </style>
